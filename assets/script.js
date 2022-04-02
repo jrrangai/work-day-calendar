85fc583f36f979036351b863de695c239b5f3811
Joshua Rangai <rangaij0116@hotmail.com>
2022-04-02 18:51:59 -0500

    final touches

--- assets/script.js
+++ assets/script.js
@@ -19,11 +19,6 @@ var hoursOfOpperation = [
 var currentHour = moment().format('H');
 
 console.log(moment().format('H'));
-// Do I create the task outside the busincessHours function or within
-    // Do I create it and then the business hours function runs it? 
-    // Should the button.on be inside the business hours function or a new one
-// How do I load locally stored data
-// Audit the tasks to show past, present, and future
 
 // I am presented with time blocks for standard business hours
 function businessHours() {
@@ -31,11 +26,13 @@ function businessHours() {
         // Hours
         var timeRow = $('<div>').addClass('row time-block');
         var hour = $('<p>').addClass('hour col-lg-3').text(hoursOfOpperation[i]);
+        // get items from local storage
         var text = localStorage.getItem(hoursOfOpperation[i]);
         // Description (text)
         var taskDescription = $('<textarea>').text(text);
         // button
         var miltaryHour = i + 9;
+        // each time block is color-coded to indicate whether it is in the past, present, or future
         if (currentHour > miltaryHour) {
             taskDescription.addClass('description col-lg-7 past')
         } else if (currentHour < miltaryHour) {
@@ -57,18 +54,8 @@ function businessHours() {
             var textDescription = $(this).siblings()[1];
             textDescription = $(textDescription).val();
             localStorage.setItem(indexHour, textDescription);
-            // get items from local storage and put value line 22
         })
     }
-}
-// each time block is color-coded to indicate whether it is in the past, present, or future
-
-// WHEN I click into a time block, THEN I can enter an event
-
-// the text for that event is saved in local storage
-    // save tasks, 
-
-// WHEN I refresh the page, THEN the saved events persist
-    // load tasks
+};
 
 businessHours();
